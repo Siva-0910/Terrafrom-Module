a2e235c7c3498ea098970b8d02cbdf854752c4ad
siva-0910 <sivaramakrishnaperuboina4@gmail.com>
2026-09-23 07:03:42 +0530

    K8

--- output.tf
+++ output.tf
@@ -1,6 +1,12 @@
 output "vpc_id" {
     value = aws_vpc.roboshop.id
 }
-# output "zone_availability"{
-#     value = data.aws_availability_zones.available
-# }
+output "public_subnet_ids" {
+  value = aws_subnet.public.id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private.id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database.id
+}
